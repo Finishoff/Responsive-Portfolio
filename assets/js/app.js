7a44f2e4227e44877fcce0e64022d26bfff61c52
Finishoff <43557860+Finishoff@users.noreply.github.com>
2018-12-08 09:35:08 -0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,8 @@
 
-/*$('a.page-scroll').on('click', function(event){
+$('a.page-scroll').on('click', function(event){
 
     $('html, body').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 500);
     event.preventDefault();
 });
-
-
-Work in progress, if wanted to animate webpage. 
