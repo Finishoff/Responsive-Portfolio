aa1181c217cd43130adff974c08e14266960dfd8
Finishoff <43557860+Finishoff@users.noreply.github.com>
2018-12-07 22:09:55 -0800

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+
+/*$('a.page-scroll').on('click', function(event){
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    event.preventDefault();
+});
+
+
+Work in progress, if wanted to animate webpage. 
